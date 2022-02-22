fe35cc3d4b0d823580287878747b887bbfb4932b
bharathguntreddi3 <guntreddibharath@gmail.com>
2022-02-22 11:38:22 +0530

    22022022_a_special_date_for_scraping

---
+++ scrap.py
@@ -0,0 +1,9 @@
+import urllib.request       # import the urllib.request library to download the content of the links
+
+f = open('file_name.txt')   #file_name is the file where you copy all the extracted links 
+
+i = 1
+for line in f:
+    urllib.request.urlretrieve(line,f"D:\repo\scrap\sample_images/{i}.jpg")     #assign a folder where you want to store all the extracted images or the content with with each img name enclosed within {i}.jpg
+    i+=1
+
